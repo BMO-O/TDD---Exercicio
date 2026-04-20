c909130668762a962a09252b17d1fb532193c799
BMO-O <151739349+BMO-O@users.noreply.github.com>
2026-04-20 18:23:24 -0300

    RED: Movimento para esquerda

--- teste.py
+++ teste.py
@@ -9,3 +9,8 @@ def test_mover_para_baixo():
     cobra = Snake(pos_inicial=(5,5), direcao='s')
     cobra.mover()
     assert cobra.corpo[0] == (6, 5)
+
+def test_mover_para_esquerda():
+    cobra = Snake(pos_inicial=(5,5), direcao='a')
+    cobra.mover()
+    assert cobra.corpo[0] == (5, 4)
