36cb2f80518422f6cb476615b1577e196b2f84d4
BMO-O <151739349+BMO-O@users.noreply.github.com>
2026-04-20 17:52:07 -0300

    GREEN: Implementado movimento para cima

---
+++ snake.py
@@ -0,0 +1,9 @@
+class Snake:
+    
+    def __init__(self, pos_inicial: tuple, direcao: str):
+        self.corpo = [pos_inicial]
+        self.direcao = direcao
+
+    def mover(self):
+        row, col = self.corpo[0]
+        self.corpo[0] = (row - 1, col)
