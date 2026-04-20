47d04cdea9d3f1b0eaf304f89b817f4707f96696
BMO-O <151739349+BMO-O@users.noreply.github.com>
2026-04-20 18:17:25 -0300

    RED: Movimentar cobra para baixo

--- teste.py
+++ teste.py
@@ -4,3 +4,8 @@ def test_mover_para_cima():
     cobra = Snake(pos_inicial=(5, 5), direcao='w')
     cobra.mover()
     assert cobra.corpo[0] == (4, 5)
+
+def test_mover_para_baixo():
+    cobra = Snake(pos_inicial=(5,5), direcao='s')
+    cobra.mover()
+    assert cobra.corpo[0] == (6, 5)
