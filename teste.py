e9105614727791cf9ba686c40c458f2e2d49b3df
BMO-O <151739349+BMO-O@users.noreply.github.com>
2026-04-20 18:36:49 -0300

    RED: Adicionar teste para mudança de direção
    Está sendo levado em consideração que não deve ser permitido que a cobra vire, em um próximo movimento, para a direção oposta a atual. Isso causaria uma colisão imediata com o resto do corpo.

--- teste.py
+++ teste.py
@@ -19,3 +19,8 @@ def test_mover_para_direita():
     cobra = Snake(pos_inicial=(5,5), direcao='d')
     cobra.mover()
     assert cobra.corpo[0] == (5, 6)
+    
+def test_mover_para_direcao_valida():
+    cobra = Snake(pos_inicial=(5, 5), direcao='w')
+    cobra.mudar_direcao('d')
+    assert cobra.direcao == 'd'
